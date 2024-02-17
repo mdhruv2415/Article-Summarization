4418bbe88e00f1d105457ea439376c7c40930d36
Shrikant Tyagi <shrikanttyagi@Shrikants-MacBook-Air.local>
2024-02-17 16:32:53 +0530

    Third Commit

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:4000/api/v1"
-const BASE_URL = "https://articlesummarizatin.onrender.com"
+const BASE_URL = "https://articlesummarizatin.onrender.com/api/v1"
 console.log(BASE_URL);
 
 //Auth APIs
